e3c7f17c45d983d8e387337e6687244bedd5ffd3
Morten Olsen <fbtijfdq@void.black>
2023-06-06 12:28:30 +0200

    chore: update

---
+++ .changeset/stupid-steaks-compare.md
@@ -0,0 +1,7 @@
+---
+'@morten-olsen/template-a': patch
+'@morten-olsen/template-b': patch
+'@morten-olsen/template-c': patch
+---
+
+update mono-repo setup
---
+++ packages/tests/src/test.ts
@@ -0,0 +1,9 @@
+import b from '@morten-olsen/template-b';
+import c from '@morten-olsen/template-c';
+
+describe('test', () => {
+  it('should work', () => {
+    expect(b).toBe('ba');
+    expect(c).toBe('ca');
+  });
+});
